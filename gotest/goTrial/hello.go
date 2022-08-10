83f0cdf247c3b30d54cfa45bcca4f4e45b6ddea9
Yinebeb-01 <yintar5@gmail.com>
2022-08-10 10:41:53 +0300

    refactoring

--- gotest/goTrial/hello.go
+++ gotest/goTrial/hello.go
@@ -7,13 +7,28 @@ import (
 //once I passed the test, let I refactor by adding a constant and test agian.
 const helloPrefix = "hello"
 
+const spanish = "Hola"
+const french = "Bonjour"
+const amharic = "Selam"
+
 func hello(name, lang string) string {
 	if name == "" {
 		name = "World"
 	}
+	if lang == "sp" {
+		return spanish + "," + name
+	}
+	if lang == "fr" {
+		return french + "," + name
+	}
+	if lang == "amh" {
+		return amharic + "," + name
+	}
+	// but instead of doing such lengthy if; let I use switch()
+
 	return helloPrefix + " " + name
 }
 func main() {
-	fmt.Println(hello("Yinebeb","en"))
+	fmt.Println(hello("Yinebeb", "en"))
 
 }
--- gotest/goTrial/trial_test.go
+++ gotest/goTrial/trial_test.go
@@ -19,8 +19,8 @@ func TestTrial(t *testing.T) {
 	//@test   , subtest1  ; if test one passed but following subtest failed,
 	//the compiler will report as it fiales. no way to show the pass of subtest1
 	t.Run("Say hello to the people", func(t *testing.T) {
-		got := hello("Tina","fr") //what the code gives us
-		want := "hello Tina" //the result which we are expecting to become.
+		got := hello("Tina", "fr") //what the code gives us
+		want := "Bonjour,Tina"     //the result which we are expecting to become.
 		// if got != want {
 		// 	t.Errorf("got%q,want%q", got, want)
 		// }
@@ -36,9 +36,24 @@ func TestTrial(t *testing.T) {
 		assertmsg(t, got, want)
 	})
 
+	//@test, go on; let we refactor our code more; adding another requirement which take a languiage to and great asp.
+	//@spanish
 	t.Run("test spanish", func(t *testing.T) {
-		got := hello("Tina", "spanish")
-		want := "Spanish,Tina"
+		got := hello("Tina", "sp")
+		want := "Hola,Tina"
+		assertmsg(t, got, want)
+	})
+
+	//@french
+	t.Run("test French", func(t *testing.T) {
+		got := hello("Tina", "fr")
+		want := "Bonjour,Tina"
+		assertmsg(t, got, want)
+	})
+	//@amh
+	t.Run("test Amharic", func(t *testing.T) {
+		got := hello("Tina", "amh")
+		want := "Selam,Tina"
 		assertmsg(t, got, want)
 	})
 }
