9f0196f25260f7efd4b76895ba6d8112d004c2ca
Yinebeb-01 <yintar5@gmail.com>
2022-08-10 10:50:22 +0300

    refactoring

--- gotest/goTrial/hello.go
+++ gotest/goTrial/hello.go
@@ -5,28 +5,37 @@ import (
 )
 
 //once I passed the test, let I refactor by adding a constant and test agian.
-const helloPrefix = "hello"
 
 const spanish = "Hola"
 const french = "Bonjour"
 const amharic = "Selam"
+const def_eng = "hello"
 
 func hello(name, lang string) string {
 	if name == "" {
 		name = "World"
 	}
-	if lang == "sp" {
-		return spanish + "," + name
-	}
-	if lang == "fr" {
-		return french + "," + name
-	}
-	if lang == "amh" {
-		return amharic + "," + name
-	}
+	// if lang == "sp" {
+	// 	return spanish + "," + name
+	// }
+	// if lang == "fr" {
+	// 	return french + "," + name
+	// }
+	// if lang == "amh" {
+	// 	return amharic + "," + name
+	// }
 	// but instead of doing such lengthy if; let I use switch()
+	switch lang {
+	case "sp":
+		return spanish + "," + name
+	case "fr":
+		return french + "," + name
+	case "amh":
+		return amharic + "," + name
+	default:
+		return def_eng + " " + name
+	}
 
-	return helloPrefix + " " + name
 }
 func main() {
 	fmt.Println(hello("Yinebeb", "en"))
