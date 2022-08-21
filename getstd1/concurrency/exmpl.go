56565920dbffc3586b975f4a6ff952b76fc5c469
Yinebeb-01 <yintar5@gmail.com>
2022-08-21 12:52:22 +0300

    more on pkgs

--- getstd1/concurrency/exmpl.go
+++ getstd1/concurrency/exmpl.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"time"
 )
@@ -15,4 +16,15 @@ func getdigit(dig []int) {
 	for _, d := range dig {
 		fmt.Printf("%d at time %v\n", d, time.Since(start))
 	}
+}
+
+// exmples on pkg and functions
+func pkg() {
+
+	var buff bytes.Buffer // use this way to read/write to a bytes and strings.
+
+	byt := buff.Bytes() // to convert tbuffer named buff into slice f bytes
+
+	fmt.Println(byt)
+
 }
