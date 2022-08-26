57fbf4e0cf76e590dc8ed15de08d5ced5eafafb2
Yinebeb-01 <yintar5@gmail.com>
2022-08-26 15:12:22 +0300

    Mocking-DInjection last commit

--- gotest/mocking/count.go
+++ gotest/mocking/count.go
@@ -1,8 +1,10 @@
-package mocking
+package main
 
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
 var numlist []int
@@ -20,3 +22,39 @@ func count(num, dec int) {
 
 //In BDD's feature step definition, since it is like testing;most of implementation need to focus on assertion.
 // just practice along that.
+
+//Mocking more ...
+
+const writemsg = "write"
+const sleepmsg = "sleep"
+
+type sleeper interface {
+	sleep()
+}
+type SpySleeper struct {
+	Calls []string
+}
+
+func (s *SpySleeper) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, writemsg)
+	return
+} //here SpySleeper implement both sleeper interafe and io.writer; since it use both Write and sleep.
+//NB: If you make write, it will be wrong.
+
+func (s *SpySleeper) sleep() { // creating custom sleep func, since time.sleep staied longer.
+	s.Calls = append(s.Calls, sleepmsg)
+}
+
+func countDown(out io.Writer, sleeper sleeper) { //here general purpose writer used
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.sleep()
+	}
+	fmt.Fprint(out, "Go")
+}
+
+func main() {
+	sleeper := &SpySleeper{}
+
+	countDown(os.Stdout, sleeper) //this will send to the console
+}
