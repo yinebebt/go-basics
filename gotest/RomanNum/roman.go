93f354bb0aca54717b2805c74a92b9b06c1d6b90
Yinebeb-01 <yintar5@gmail.com>
2022-08-31 09:44:01 +0300

    roman numeral

---
+++ gotest/RomanNum/roman.go
@@ -0,0 +1,21 @@
+package RomanNum
+
+import "strings"
+
+func ConvertToRoman(arabic int) string {
+	var result strings.Builder
+	for arabic > 0 {
+		switch {
+		case arabic > 4:
+			result.WriteString("V")
+			arabic -= 5
+		case arabic > 3:
+			result.WriteString("IV")
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic--
+		}
+	}
+	return result.String()
+}
