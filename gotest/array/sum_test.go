10f13d776b83e92b949579fba0a721faf12cbc84
Yinebeb-01 <yintar5@gmail.com>
2022-09-02 11:40:14 +0300

    last commit for Go Basics with TDD

--- gotest/array/sum_test.go
+++ gotest/array/sum_test.go
@@ -82,3 +82,28 @@ func TestAllTail(t *testing.T) {
 	})
 
 }
+
+//with generics
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+
+		AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(x, y string) string {
+			return x + y
+		}
+
+		AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
+}
+
+func AssertEqual(t testing.TB,got, want interface{}){
+	if got!=want{
+		t.Errorf("got %v want %v", got, want)
+	}
+}
