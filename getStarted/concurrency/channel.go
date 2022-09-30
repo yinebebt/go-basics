1ea3bfcbab9fd7a8824768459f1e79ca1985841a
Yinebeb-01 <yintar5@gmail.com>
2022-09-30 09:24:56 +0300

    concurrency added

---
+++ getStarted/concurrency/channel.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func channel(name chan string) {
+	var a chan int // empty/nil channel which has no any use,
+	fmt.Println("value of empty channnel:", a)
+	mychan := make(chan int) // this chan, formed via make comes useful to pass data
+
+	fmt.Println("value of make channel: ", mychan)
+
+	fmt.Println("hello" + <-name) // passing data from channel to stdout.
+	//let we see effect of closing a channel
+	<-name
+}
+
+// square - more on channel
+func square(c chan int) {
+	for i := 0; i < 10; i++ {
+		c <- i * i //sending square of i to the channel, to retrieve later via a goroutine
+		fmt.Println("from go routine")
+	}
+	close(c)
+}
+
+// channel with buffer size
+func sum(num chan int) {
+	fmt.Println("from sum routine")
+	for i := 0; i < 3; i++ {
+		n := <-num
+		fmt.Println(n * n)
+	}
+}
